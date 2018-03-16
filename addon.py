e97664f311585b4616c4595bf2686bb06cd6cebc
johnwilliamson <johnwilliamson@johns-air-2575.localdomain>
2018-03-16 15:27:15 +0000

    Less lines to make the dialog more compact

--- addon.py
+++ addon.py
@@ -1,25 +1,26 @@
 import pyipinfoio
 import xbmcgui
-import pydevd
+#import pydevd
 from json import load
 from urllib2 import urlopen
 
-pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
+#pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
 
 my_ip = load(urlopen('http://jsonip.com'))['ip']
 ip = pyipinfoio.IPLookup()
+lookup = ip.lookup(my_ip)
 
-line1 = 'ORG: ' + (ip.lookup(my_ip))['org']
-line2 = 'CITY: ' + (ip.lookup(my_ip))['city']
-line3 = 'COUNTRY: ' +(ip.lookup(my_ip))['country']
-line4 = 'REGION: ' +(ip.lookup(my_ip))['region']
-line5 = 'HOST: ' +(ip.lookup(my_ip))['hostname']
-line6 = 'IP: ' +(ip.lookup(my_ip))['ip']
+line1 = 'ORG: ' + lookup['org']
+line2 = 'CITY: ' + lookup['city']
+#line3 = 'COUNTRY: ' +(ip.lookup(my_ip))['country']
+line4 = 'REGION: ' +lookup['region']
+line5 = 'HOST: ' +lookup['hostname']
+#line6 = 'IP: ' +(ip.lookup(my_ip))['ip']
 
 
 #xbmcgui.Dialog().ok('Current Geolocation Info', line1+'\n'+ line2+'\n'+  line3+'\n'+  line4+'\n'+  line5+'\n'+  line6)
 
-xbmcgui.Dialog().textviewer('Current Geolocation Info', line1+'\n'+ line2+'\n'+  line3+'\n'+  line4+'\n'+  line5+'\n'+  line6)
+xbmcgui.Dialog().ok('Current Geolocation Info', line1+'\n'+ line2+'\n'+   line4+'\n'+  line5)
 
 #xbmcgui.Dialog().ok('Current Geolocation Info', line1, line2, line3, line4, line5, line6)
 
