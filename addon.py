8893e5d8eebcf080e54fb889043ace70d6024c41
johnwilliamson <johnwilliamson@johns-air-2575.localdomain>
2018-03-16 15:53:57 +0000

    Try with a yes/no dialog

--- addon.py
+++ addon.py
@@ -1,14 +1,32 @@
 import pyipinfoio
+import subprocess
 import xbmcgui
-#import pydevd
+import pydevd
 from json import load
 from urllib2 import urlopen
 
-#pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
+pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
 
 my_ip = load(urlopen('http://jsonip.com'))['ip']
 ip = pyipinfoio.IPLookup()
 lookup = ip.lookup(my_ip)
 
-xbmcgui.Dialog().ok('Current Geolocation Info', 'ORG: ' + lookup['org'] +'\n'+ 'CITY: ' + lookup['city']+'\n'+   'REGION: ' +lookup['region']+'\n'+  'HOST: ' +lookup['hostname'])
+info = 'ORG: ' + lookup['org'] +'\n'+ 'CITY: ' + lookup['city']+'\n'+   'REGION: ' +lookup['region']+'\n'+  'HOST: ' +lookup['hostname']
+
+print('Current Geolocation Info', info)
+
+
+dialog = xbmcgui.Dialog()
+if dialog.yesno('Current Geolocation Info', info,'Start VPN?'):
+	print 'Going to start VPN'
+else:
+	print 'Not going to start VPN'
+
+
+#openvpn_cmd = ['sudo', 'openvpn', '--config', 'client.cfg', '--auth-user-pass', 'hmaauth.conf']
+#prog = subprocess.Popen(openvpn_cmd)
+
+#prog.
+
+#xbmcgui.Dialog().ok('Current Geolocation Info', 'ORG: ' + lookup['org'] +'\n'+ 'CITY: ' + lookup['city']+'\n'+   'REGION: ' +lookup['region']+'\n'+  'HOST: ' +lookup['hostname'])
 
