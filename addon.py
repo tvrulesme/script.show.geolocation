b0d76af5f8b6505cc67f027a537fef3906a2f8df
johnwilliamson <johnwilliamson@johns-air-2575.localdomain>
2018-03-16 14:59:50 +0000

    Adding Geolocation sample code

--- addon.py
+++ addon.py
@@ -1,46 +1,21 @@
-#import xbmcaddon
-#import xbmcgui
-import os
-import xbmc
-import time
-import threading
-#import pydevd
+import pyipinfoio
+import xbmcgui
+import pydevd
+from json import load
+from urllib2 import urlopen
+
+pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
+
+my_ip = load(urlopen('http://jsonip.com'))['ip']
+ip = pyipinfoio.IPLookup()
+
+line1 = 'ORG: ' + (ip.lookup(my_ip))['org']
+print(ip.lookup(my_ip))['city']
+print(ip.lookup(my_ip))['country']
+print(ip.lookup(my_ip))['region']
+print(ip.lookup(my_ip))['hostname']
+print(ip.lookup(my_ip))['ip']
 
 
-global hasRun
+xbmcgui.Dialog().ok('Current Geolocation Info', line1)
 
-def yesNoDialog():
-		while not hasRun:
-			if xbmc.getCondVisibility("Window.isVisible(yesnodialog)"):
-				print 'clicking yesnodialog'
-				xbmc.executebuiltin('SendClick(11)')
-			else:
-				time.sleep(0.5)
-				
-def sendCommand(res):
-	xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue", "params":{"setting":"videoscreen.resolution","value":'+res+'},"id":1}')
-
-#pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
-
-tempdir = xbmc.translatePath('special://temp/')
-tempfile0 = os.path.join(tempdir, 'reslutiontoggle0')
-
-
-hasRun = False
-
-t1 = threading.Thread(target=yesNoDialog)
-t1.start()
-
-if not os.path.isfile(tempfile0):
-	print 'Switching to full screen resolution'
-	sendCommand('17')
-	tempfile = open(tempfile0, "a")
-	tempfile.close()
-	
-else:
-	print 'Switching to projector resolution'
-	sendCommand('20')
-	os.remove(tempfile0)
-
-hasRun = True	
-t1.join(3)
