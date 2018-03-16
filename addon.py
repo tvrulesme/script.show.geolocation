885368f7494d102c2cb34ea916de9349d39c2aa1
johnwilliamson <johnwilliamson@johns-air-2575.localdomain>
2018-03-16 15:34:16 +0000

    Minor refactoring

--- addon.py
+++ addon.py
@@ -10,17 +10,5 @@ my_ip = load(urlopen('http://jsonip.com'))['ip']
 ip = pyipinfoio.IPLookup()
 lookup = ip.lookup(my_ip)
 
-line1 = 'ORG: ' + lookup['org']
-line2 = 'CITY: ' + lookup['city']
-#line3 = 'COUNTRY: ' +(ip.lookup(my_ip))['country']
-line4 = 'REGION: ' +lookup['region']
-line5 = 'HOST: ' +lookup['hostname']
-#line6 = 'IP: ' +(ip.lookup(my_ip))['ip']
-
-
-#xbmcgui.Dialog().ok('Current Geolocation Info', line1+'\n'+ line2+'\n'+  line3+'\n'+  line4+'\n'+  line5+'\n'+  line6)
-
-xbmcgui.Dialog().ok('Current Geolocation Info', line1+'\n'+ line2+'\n'+   line4+'\n'+  line5)
-
-#xbmcgui.Dialog().ok('Current Geolocation Info', line1, line2, line3, line4, line5, line6)
+xbmcgui.Dialog().ok('Current Geolocation Info', 'ORG: ' + lookup['org'] +'\n'+ 'CITY: ' + lookup['city']+'\n'+   'REGION: ' +lookup['region']+'\n'+  'HOST: ' +lookup['hostname'])
 
