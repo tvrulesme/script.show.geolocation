1e3be31cba247b880d42e892d7f52560e066b04f
johnwilliamson <johnwilliamson@johns-air-2575.localdomain>
2018-03-17 06:03:49 +0000

    adding a shutdown for vpn

--- testaddon.py
+++ testaddon.py
@@ -17,8 +17,13 @@ info = 'ORG: ' + lookup['org'] +'\n'+ 'CITY: ' + lookup['city']+'\n'+   'REGION:
 #print('Current Geolocation Info', info)
 print(info)
 
-
-p = subprocess.Popen('sudo openvpn --config /home/john/openvpn/ipvanish-UK-London-lon-a48.conf', shell=True, stdout=None, stderr=None, preexec_fn=os.setpgrp)
-print(p.pid)
+if 'Virgin' in info: 
+	p = subprocess.Popen('sudo openvpn --config /home/john/openvpn/ipvanish-UK-London-lon-a48.conf', shell=True, stdout=None, stderr=None, preexec_fn=os.setpgrp)
+	print(p.pid)
+else:
+	CREATE_NEW_PROCESS_GROUP = 0x00000200
+	DETACHED_PROCESS = 0x00000008
+	openvpn_cmd = ['sudo', 'killall', 'openvpn']
+	p = subprocess.Popen('sudo killall openvpn', shell=True, stdout=None, stderr=None, preexec_fn=os.setpgrp)
 
 
