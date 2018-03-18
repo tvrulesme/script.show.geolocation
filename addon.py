74eadbb8f75ee063e2783e804ea574281be5e282
johnwilliamson <johnwilliamson@johns-air-2575.localdomain>
2018-03-18 07:49:49 +0000

    Moving start stop to a new function

--- addon.py
+++ addon.py
@@ -8,6 +8,15 @@ from urllib2 import urlopen
 import xbmc
 import subprocess
 
+
+def startStopVpn(updown):
+	passDialog = xbmcgui.Dialog()
+	password = passDialog.input('Enter sudo Password', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
+
+	process = subprocess.Popen('sudo -S nmcli con ' + updown + ' id ipvanish-UK-London-lon-a48', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+	process.communicate(password + '\n')[1]
+
+
 pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
 
 my_ip = load(urlopen('http://jsonip.com'))['ip']
@@ -16,36 +25,24 @@ lookup = ip.lookup(my_ip)
 
 info = 'ORG: ' + lookup['org'] +'\n'+ 'CITY: ' + lookup['city']+'\n'+   'REGION: ' +lookup['region']+'\n'+  'HOST: ' +lookup['hostname']
 
-#print('Current Geolocation Info', info)
+print('Current Geolocation Info', info)
 
 dialog = xbmcgui.Dialog()
 if 'Virgin' not in info: 
 	if dialog.yesno('VPN connected', info,'Stop VPN?'):
 		print 'Going to stop VPN'
-		xbmc.executebuiltin('XBMC.RunScript(script.openvpn,disconnect)')
+		startStopVpn('down')
 	else:
 		print 'Not going to stop VPN'
 else:	
 	if dialog.yesno('VPN not connected', info,'Start VPN?'):
 		print 'Going to start VPN'
-		#passDialog = xbmcgui.Dialog.input('Enter secret code', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
-		passDialog = xbmcgui.Dialog()
-		password = passDialog.input('Enter secret code', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
-		
-		
-		#d = passDialog.input('Enter secret code', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
-		print(password)
-		
-		process = subprocess.Popen('sudo -S nmcli con up id ipvanish-UK-London-lon-a48', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+		startStopVpn('up')
+		#passDialog = xbmcgui.Dialog()
+		#password = passDialog.input('Enter sudo Password', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
 
-		sudo_prompt = process.communicate(password + '\n')[1]
-		
-		
-		
-		#sudo nmcli con down id ipvanish-UK-London-lon-a48
-		
-		#RunScript(script.openvpn, disconnect)
-		xbmc.executebuiltin('XBMC.RunScript(script.openvpn,ipvanish)')
+		#process = subprocess.Popen('sudo -S nmcli con up id ipvanish-UK-London-lon-a48', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+		#sudo_prompt = process.communicate(password + '\n')[1]
 	else:
 		print 'Not going to start VPN'
 
