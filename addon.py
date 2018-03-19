870abae6f52ae3367ac0943f1ca6718e119ba86e
johnwilliamson <johnwilliamson@johns-air-2575.localdomain>
2018-03-19 15:06:07 +0000

    Make connection test more reliable

--- addon.py
+++ addon.py
@@ -5,7 +5,7 @@ import xbmcaddon
 from json import load
 from urllib2 import urlopen
 import subprocess
-#import pydevd
+import pydevd
 import os
 
 green = '[COLOR forestgreen]'
@@ -17,17 +17,20 @@ endColor = '[/COLOR]'
 def getVpnList():
 	p = subprocess.Popen(["nmcli","con"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 	outputText =  p.communicate()[0]
+	vpnConnected = False
 	for line in outputText.split('\n'):
 		splitLine = line.split()
 		if(splitLine and splitLine[2] == 'vpn'):
 			if(splitLine[3] != '--'):
 				vpnlistdisplay.append(green + splitLine[0] + endColor + ' - Connected')
+				vpnConnected = True
 			else:
 				vpnlistdisplay.append(red + splitLine[0] + endColor + ' - Disconnected')
 			vpnlist.append(splitLine[0])
+	return vpnConnected
 
 			
-def showVpnInfo():
+def showVpnInfo(connected):
 	iconpath = xbmcaddon.Addon().getAddonInfo('path').decode("utf-8") + "/icon.png"
 	my_ip = load(urlopen('http://jsonip.com'))['ip']
 	ip = pyipinfoio.IPLookup()
@@ -35,16 +38,16 @@ def showVpnInfo():
 	#message = 'ORG: ' + lookup['org'] +'\n'+ 'CITY: ' + lookup['city']+'\n'+   'REGION: ' +lookup['region']+'\n'+  'HOST: ' +lookup['hostname']
 	org = lookup['org']
 	#print(lookup)
-	message = green + org + endColor if 'Virgin' not in org else red + org + endColor
-	header = 'Connected to VPN' if 'Virgin' not in message else 'Disconnected from VPN'
+	message = green + org + endColor if connected else red + org + endColor
+	header = 'Connected to VPN' if connected else 'Disconnected from VPN'
 	xbmc.executebuiltin('Notification(' + header + ',' + message + ',5000,' + iconpath+ ')')
 
-#pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
+pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
 
 vpnlistdisplay = [] 
 vpnlist = [] 
 
-getVpnList()
+connected = getVpnList()
 
 #print vpnlist
 
@@ -53,7 +56,7 @@ selectedVpn = dialog.select('Select vpn',vpnlistdisplay)
 
 if selectedVpn >= 0:
 	password = ''
-	connected = True if green in vpnlistdisplay[selectedVpn] else False
+	connectDisconnect = 'disconnect' if connected else 'connect'
 	password = xbmcgui.Dialog().input('Enter password to disconnect VPN', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
 	
 	if password:
@@ -63,6 +66,8 @@ if selectedVpn >= 0:
 		process.communicate(password + os.linesep)[1]
 	
 
-showVpnInfo()
+connected = getVpnList()
+
+showVpnInfo(connected)
 
 print 'done'
