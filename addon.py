4719642ce9efd216c11d4888fa0554c55ef1ccba
johnwilliamson <johnwilliamson@johns-air-2575.localdomain>
2018-03-19 14:45:52 +0000

    Code cleanup and debug remove

--- addon.py
+++ addon.py
@@ -5,10 +5,14 @@ import xbmcaddon
 from json import load
 from urllib2 import urlopen
 import subprocess
-import pydevd
+#import pydevd
 import os
-#import resources.lib.kodisettings as settings
-#import sys
+
+green = '[COLOR forestgreen]'
+red = '[COLOR red]'
+endColor = '[/COLOR]'
+#vpnConnected = False
+
 
 def getVpnList():
 	p = subprocess.Popen(["nmcli","con"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -17,10 +21,11 @@ def getVpnList():
 		splitLine = line.split()
 		if(splitLine and splitLine[2] == 'vpn'):
 			if(splitLine[3] != '--'):
-				vpnlistdisplay.append('[COLOR forestgreen]' + splitLine[0] + '[/COLOR] - Connected')
+				vpnlistdisplay.append(green + splitLine[0] + endColor + ' - Connected')
 			else:
-				vpnlistdisplay.append('[COLOR red]' + splitLine[0] + '[/COLOR] - Disconnected')
+				vpnlistdisplay.append(red + splitLine[0] + endColor + ' - Disconnected')
 			vpnlist.append(splitLine[0])
+
 			
 def showVpnInfo():
 	iconpath = xbmcaddon.Addon().getAddonInfo('path').decode("utf-8") + "/icon.png"
@@ -29,43 +34,15 @@ def showVpnInfo():
 	lookup = ip.lookup(my_ip)
 	#message = 'ORG: ' + lookup['org'] +'\n'+ 'CITY: ' + lookup['city']+'\n'+   'REGION: ' +lookup['region']+'\n'+  'HOST: ' +lookup['hostname']
 	org = lookup['org']
-	print(lookup)
-	message = '[COLOR forestgreen]' + org + '[/COLOR]' if 'Virgin' not in org else '[COLOR red]' + org + '[/COLOR]'
-	connected = 'Connected to VPN' if 'Virgin' not in message else 'Disconnected from VPN'
-	xbmc.executebuiltin('Notification(' + connected + ',' + message + ',5000,' + iconpath+ ')')
+	#print(lookup)
+	message = green + org + endColor if 'Virgin' not in org else red + org + endColor
+	header = 'Connected to VPN' if 'Virgin' not in message else 'Disconnected from VPN'
+	xbmc.executebuiltin('Notification(' + header + ',' + message + ',5000,' + iconpath+ ')')
 
-
-	
-
-pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
+#pydevd.settrace('192.168.0.55', stdoutToServer=True, stderrToServer=True)
 
 vpnlistdisplay = [] 
 vpnlist = [] 
-
-showVpnInfo()
-
-showVpnInfo()
-
-# Set some global values.
-#_addonid = 'script.show.geolocation'
-
-#image = _settings.get_path('icon.png')
-
-#xbmc.translatePath('%s/%s' % (self.__path__, path))
-
-
-
-
-#icon=os.path.join(addon.getAddonInfo('path'), 'resources','skins','Default','media', icon)
-
-
-	
-
-
-
-
-
-
 
 getVpnList()
 
@@ -76,7 +53,7 @@ selectedVpn = dialog.select('Select vpn',vpnlistdisplay)
 
 if selectedVpn >= 0:
 	password = ''
-	connected = True if 'forestgreen' in vpnlistdisplay[selectedVpn] else False
+	connected = True if green in vpnlistdisplay[selectedVpn] else False
 	password = xbmcgui.Dialog().input('Enter password to disconnect VPN', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
 	
 	if password:
@@ -86,28 +63,6 @@ if selectedVpn >= 0:
 		process.communicate(password + os.linesep)[1]
 	
 
-
-
-
-#print vpnlist[selectedVpn]
-
-
-# if 'Virgin' not in info: 
-# 	passDialog = xbmcgui.Dialog()
-# 	password = passDialog.input('[COLOR forestgreen]' + info + '[/COLOR] enter password to disconnect VPN', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
-# 	if password:
-# 		process = subprocess.Popen('sudo -S nmcli con down id ipvanish-UK-London-lon-a48', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-# 		process.communicate(password + '\n')[1]
-# 		
-# 		process = subprocess.Popen('sudo -S nmcli con down id ipvanish-UK-London-lon-a48', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-# 
-# else:	
-# 	passDialog = xbmcgui.Dialog()
-# 	password = passDialog.input('[COLOR red]' + info + '[/COLOR] enter password to connect VPN', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
-# 	#print('#' + password + '#')
-# 	if password:
-# 		process = subprocess.Popen('sudo -S nmcli con up id ipvanish-UK-London-lon-a48', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-# 		process.communicate(password + '\n')[1]
-
+showVpnInfo()
 
 print 'done'
